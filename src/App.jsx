cd4622cd7467796669acdb2f5965036cd93b9eb0
Ishu Singh <ishusingh058@gmail.com>
2023-05-10 21:42:01 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Burger from './components/Burger';
+
+function App() {
+
+  return (
+    <> 
+    <div className='burger-css'> <Burger/></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Burger.jsx
@@ -0,0 +1,13 @@
+export default function Burger() {
+  // const [lettuce, setLetuce] = useState(0)
+  // const [tomato, setTomato] = useState(0)
+  // const [onion, setOnion] = useState(0)
+  // const [cheese, setCheese] = useState(0)
+  // const [patty, setPatty] = useState(0)
+  // 
+  return (
+    <div>
+      <h1>Zingy Burger Maker</h1>
+    </div>
+  )
+}
